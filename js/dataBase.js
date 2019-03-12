d2c8ab9efb59b9667165d7074cd1dc1b5eff8b28
Tomeu Marqueño <tmarqueno@bitgrup.com>
2019-03-12 09:21:11 +0100

    0.1.3

--- js/dataBase.js
+++ js/dataBase.js
@@ -11,7 +11,7 @@ var dataBase = {
             dataBase.createTables();
             return true;
         } catch (e) {
-            if(bitgrup.production){
+            if (bitgrup.production) {
                 $('#title-page-error').html('Aquest dispositiu no es compatible amb l\'app, disculpi les molèsties.');
                 bitgrup.changePage('noCompatible');
                 bitgrup.initScreen();
@@ -19,57 +19,62 @@ var dataBase = {
             }
         }
     },
-    
+
     createTables: function () {
         //CREATE TABLE ISSUES
         dataBase.query('CREATE TABLE IF NOT EXISTS ISSUES (ID unique, FK_ENTITY, TYPE,DESCRIPTION,DATE,HOUR,STATUS,LATITUDE,LONGITUDE,ADDRESS,LOCATION)');
         //CREATE TABLE PICTURES
         dataBase.query('CREATE TABLE IF NOT EXISTS PICTURES (ID unique, FK_ISSUE, BASE_64)');
         //CREATE TALBE CONFIG
-        dataBase.query('CREATE TABLE IF NOT EXISTS CONFIG (ID unique, ENTITY_ID, EMAIL)', function(result){
+        dataBase.query('CREATE TABLE IF NOT EXISTS CONFIG (ID unique, ENTITY_ID, EMAIL)', function (result) {
             dataBase.configInit();
         });
-        
+
     },
-    
-    configInit: function(){
-        dataBase.query('SELECT * FROM CONFIG WHERE ID = ? ', [1], function(result){
-            if(result.length === 0){
+
+    configInit: function () {
+        dataBase.query('SELECT * FROM CONFIG WHERE ID = ? ', [1], function (result) {
+            if (result.length === 0) {
                 //INSERT
-                dataBase.query('INSERT INTO CONFIG (ID) VALUES(?)', [1], function(){
+                dataBase.query('INSERT INTO CONFIG (ID) VALUES(?)', [1], function () {
                     //INICIAM API (COMUNICACIÓ AMB TIC)
                     api.init();
                 });
-            }else{
+            } else {
                 api.init();
             }
         });
     },
 
     query: function (sql, inputs, callback) {
-        if (!inputs) {
-            inputs = new Array();
-        }
-        var result = new Array();
-        dataBase.db.transaction(function (tx) {
-            tx.executeSql(sql, inputs,
-                    function (tx, result_query) {
-                        var len = result_query.rows.length;
-                        for (var i = 0; i < len; i++) {
-                            result.push(result_query.rows.item(i));
+        try {
+            if (!inputs) {
+                inputs = new Array();
+            }
+            var result = new Array();
+            dataBase.db.transaction(function (tx) {
+                tx.executeSql(sql, inputs,
+                        function (tx, result_query) {
+                            var len = result_query.rows.length;
+                            for (var i = 0; i < len; i++) {
+                                result.push(result_query.rows.item(i));
+                            }
+                            if (callback) {
+                                callback(result);
+                            }
+                        },
+                        function (transaction, error) {
+                            //bitgrup.error_('ERROR-DB-49', '', sql + '----' + error.message);
+                            console.log(sql, inputs, error);
+                            callback(false);
                         }
-                        if (callback) {
-                            callback(result);
-                        }
-                    },
-                    function (transaction, error) {
-                        //bitgrup.error_('ERROR-DB-49', '', sql + '----' + error.message);
-                        console.log(sql, inputs, error);
-                        callback(false);
-                    }
-            );
+                );
 
-        });
+            });
+        } catch (e) {
+            console.log(sql, inputs);
+            return false;
+        }
     }
 
 }
