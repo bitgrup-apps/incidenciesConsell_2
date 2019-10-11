fe877fd55eb2fc530790e7f807219dfe613eaa77
Arnau Llull <allull@bitgrup.com>
2019-10-11 13:29:41 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -854,7 +854,8 @@ var bitgrup = {
             $('#card-news-title').html(new_.title);
             $('#card-news-description').html(new_.description);
             var img = (new_.img) ? new_.img : 'images/news-background-pred.jpg';
-            $('#card-news-img').html('<img src="' + img + '" title="' + new_.title + '" alt="TIC"/>');
+            //$('#card-news-img').html('<img src="' + img + '" title="' + new_.title + '" alt="TIC"/>');
+            $('#card-news-img').html('<a data-fancybox="gallery" data-options=\'{"showNavArrows" : "true", "arrows" : "true", "buttons" : ["close"]}\' href="' + img + '"><span class="" style="background-image: url(\'' + img + '\')"><span></a>');
             $('#card-news-twitter').data('url', 'https://twitter.com/home?status=' + new_.uri);
             $('#card-news-facebook').data('url', 'https://www.facebook.com/sharer/sharer.php?u=' + new_.uri);
             $('#card-news-whatsapp').data('url', 'whatsapp://send?text=' + new_.uri);
